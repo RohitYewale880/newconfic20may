f008310298a77312fc5eb843bda539eefa59b7cc
RohitYewale880 <rohityewale2020@gmail.com>
2026-05-20 18:10:43 +0530

    confic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatIconModule} from '@angular/material/icon';
+import { TodoDashboardComponent } from './shaired/comp/todo-dashboard/todo-dashboard.component';
+import { TodoListComponent } from './shaired/comp/todo-list/todo-list.component';
+import { TodoFormComponent } from './shaired/comp/todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoDashboardComponent,
+    TodoListComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shaired/consts/todo.ts
@@ -0,0 +1,40 @@
+import { Itodo } from "../modal/todo";
+
+
+export const todoArr :Array<Itodo>= [
+    {
+        todoId: 'todo_101',
+        todoItem: 'Angular',
+        isConfirm: true
+    },
+    {
+        todoId: 'todo_102',
+        todoItem: 'JavaScript',
+        isConfirm: false
+    },
+    {
+        todoId: 'todo_103',
+        todoItem: 'CSS',
+        isConfirm: true
+    },
+    {
+        todoId: 'todo_104',
+        todoItem: 'Angular lifecycle hooks',
+        isConfirm: false
+    },
+    {
+        todoId: 'todo_105',
+        todoItem: 'Angular Material',
+        isConfirm: true
+    },
+    {
+        todoId: 'todo_107',
+        todoItem: 'snackbar',
+        isConfirm: true
+    },
+    {
+        todoId: 'todo_108',
+        todoItem: 'Bootstrap',
+        isConfirm: false
+    }
+];
